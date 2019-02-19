5b400d8670f4973d09953af377d98be88747333b
fpdotmonkey <me@fletcherporter.com>
2019-02-18 21:12:02 -0800

    added help strings and caught an issue with the two polygon collision function that looked like it could result in issues but didn't cause any issues in the tests I ran (it still doesn't cause issues

--- collisionDetection.py
+++ collisionDetection.py
@@ -5,6 +5,8 @@
 #
 
 def isPoint(possiblePoint):
+    """
+Returns true if possiblePoint is an iterable thing with length 2 whose elements are all real-valued numbers.  Complex numbers and strings will result in a False result."""
     try:
         if (len(possiblePoint) == 2
             and not isinstance(possiblePoint, str)
@@ -19,6 +21,8 @@ def isPoint(possiblePoint):
         return False
 
 def isSegment(possibleSegment):
+    """
+Returns true if possible segment is an iterable thing with length 2 whos elements are both points as defined by isPoint()."""
     try:
         if (len(possibleSegment) == 2
             and all(isPoint(point) for point in possibleSegment)):
@@ -32,6 +36,8 @@ def isSegment(possibleSegment):
         return False
 
 def isPolygon(possiblePolygon):
+    """
+Returns True if possiblePolygon is an iterable thing with length greater than zero whose elements are all points as defined by isPoint()."""
     try:
         if (len(possiblePolygon) > 0
             and all(isPoint(point) for point in possiblePolygon)):
@@ -45,7 +51,11 @@ def isPolygon(possiblePolygon):
         return False
 
 
-def isInAABB(point, polygon, smallAmount=0.01):
+def isInAABB(point, polygon, smallAmount=1e-5):
+    """
+Checks if point is inside the smallest axis-aligned bounding box (AABB) that contains polygon.  It has an optional parameter smallAmount which is the size of the small buffer between the bounding box and the polygon which is to avoid any false negatives for collision.
+
+It will raise an AssertionError if point is not a point defined by isPoint() or if polygon is not a polygon as defined by isPolygon()."""
     assert isPoint(point), "isInAABB: point is not a numerical list with two elements"
     assert isPolygon(polygon), "isInAABB: polygon is not a list of points"
 
@@ -63,6 +73,10 @@ def isInAABB(point, polygon, smallAmount=0.01):
         return False
 
 def pointOutsideOf(polygon, smallAmount=0.01):
+    """
+This returns a point that is gurenteed to be outside of polygon, useful for pretending to cast rays out of polygons.  It has an optional parameter smallAmount which is the size of the distance from the closest point that might be on the polygon.
+
+It will raise an AssertionError if polygon is not a polygon as defined by isPolygon()."""
     assert isPolygon(polygon), "pointOutsideOf: polygon is not a list of points"
 
     maxX = max(i for [i, j] in polygon) + smallAmount
@@ -71,6 +85,8 @@ def pointOutsideOf(polygon, smallAmount=0.01):
     return [ maxX, maxY ]
 
 def isOdd(n):
+    """
+Returns True if n is odd, False otherwise."""
     if (n % 2 == 1):
         
         return True
@@ -79,6 +95,10 @@ def isOdd(n):
         return False
     
 def isPointInPolygon(point, polygon):
+    """
+Checks if point is inside polygon by first checking if point is inside the smallest axis-aligned bounding box around polygon.  If not, it casts a ray from point and counting how many times it intersects the sides of the polygon.  If odd, point is inside the polygon, otherwise it's not.
+
+Raises an AssertionError is point is not a point as defined by isPoint() or if polygon is not a polygon as defined by isPolygon()."""
     assert isPoint(point), "isPointInPolygon: point is not a numerical list with two elements"
     assert isPolygon(polygon), "isPointInPolygon: polygon is not a list of points"
 
@@ -104,6 +124,10 @@ def isPointInPolygon(point, polygon):
 
 
 def doTwoSegmentsIntersect(segment1, segment2):
+    """
+Returns True if segments1 and segment2 intersect at at least one point.  It does this by computing whether the lines that contain each of segment1 and segment2 intersect using (4.2) in Bullo and Smith.  If they do, they are either co-linear or intersect at a point.  For the point case, it needs to be checked if the point on the line where the intersection occured is also on the segment.  For the co-linear case, an AABB is drawn around segment 2, and if a point on segment1 is in that box, then they must intersect.
+
+Raises an AssertionError if segment1 or segment2 are not segments as defined by isSegment()."""
     assert isSegment(segment1), "doTwoSegmentsIntersect: segment1 is not a list of 2 points"
     assert isSegment(segment2), "doTwoSegmentsIntersect: segment2 is not a list of 2 points"
     
@@ -125,7 +149,6 @@ def doTwoSegmentsIntersect(segment1, segment2):
 
         return False
     elif (numerator_a == 0 and denominator == 0):
-
         if (isInAABB(segment1[0], segment2)
             or isInAABB(segment1[1], segment2)):
             
@@ -150,6 +173,10 @@ def doTwoSegmentsIntersect(segment1, segment2):
 
 
 def doTwoConvexPolygonsIntersect(polygon1, polygon2):
+    """
+Checks if two polygons intersects by checking if any of the points in either of the polygons are in the other and if any of the segments that make up the polygons intersect.
+
+Raises an AssertionError if polygon1 or polygon2 are not polygons as defined by isPolygon()."""
     assert isPolygon(polygon1), "doTwoConvexPolygonsIntersect: polygon1 is not a list of points"
     assert isPolygon(polygon2), "doTwoConvexPolygonsIntersect: polygon1 is not a list of points"
 
@@ -160,8 +187,9 @@ def doTwoConvexPolygonsIntersect(polygon1, polygon2):
 
         for j in range(len(polygon2)):
             if (doTwoSegmentsIntersect([polygon1[i], polygon1[i-1]],
-                                       [polygon2[j], polygon2[j-1]])):
-
+                                       [polygon2[j], polygon2[j-1]])
+                or isPointInPolygon(polygon2[j], polygon1)):
+                
                 return True
     
     return False
@@ -255,7 +283,7 @@ if "__main__" == __name__:
     intersectsUnitSquare = [[0, 0], [2, 0], [2, 2], [0, 2]]
     subsetOfUnitSquare = [[0.5, 0.5], [-0.5, 0.5], [-0.5, -0.5], [0.5, -0.5]]
     supersetOfUnitSquare = [[2, 2], [-2, 2], [-2, -2], [2, -2]]
-    noVerticesIntersect = [[1.5, 0], [0, 1.5], [-1.5, 0], [0, -1.5]]
+    noVerticesIntersect = [[1.1, 0], [0, 1.1], [-1.1, 0], [0, -1.1]]
 
     print(doTwoConvexPolygonsIntersect(unitSquare,
                                        doesntIntersectUnitSquare)) # False
